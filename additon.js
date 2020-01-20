29af154f38d14f775a43cba8d1075e15946ca216
Prathipa <mprathipa@gmail.com>
2020-01-20 12:09:02 +0800

    Initial release

---
+++ additon.js
@@ -0,0 +1,5 @@
+var exports = module.exports = {};
+exports.addition = function(a,b){
+    c=a+b;
+    console.log("After Addition:"+ c);
+}
---
+++ index.js
@@ -0,0 +1,26 @@
+//Server created using http
+
+/*var http = require('http');
+
+var server = http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end('Hello World');
+});
+
+server.listen(8000,function(){
+    console.log("Server running in PORT:8000");
+});*/
+
+//Server created using express
+
+var express = require('express');
+var app = express();
+var add = require('./additon.js');
+add.addition(109,8190);
+app.set('view Engine','jade');
+app.get('/',function(req,res){
+    res.end("Hello World using express");
+});
+app.listen(8000,function(){
+    console.log("Server running in PORT:8000");
+});
